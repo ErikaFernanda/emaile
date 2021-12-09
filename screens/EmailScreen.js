44915a13178d263c6b2392f6e57c833c249e1459
Erika <erikasolivee@gmail.com>
2021-12-09 14:24:19 -0300

    adiciona page de visualização de email

--- screens/EmailScreen.js
+++ screens/EmailScreen.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Image, StyleSheet, Text, View } from "react-native";
 import { FlatList } from "react-native-gesture-handler";
 import WebView from "react-native-webview";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 export default function EmailScreen({ route }) {
   const { id } = route.params;
@@ -16,36 +17,56 @@ export default function EmailScreen({ route }) {
       const email = await response.json();
       console.log("https://mobile.ect.ufrn.br:3002/emails/" + id);
       setEmail(email);
-      
     }
     getData();
   }, []);
-
-  //   function renderItem({ item }) {
-  //     return (
-  //       <View>
-  //         <Text>{item.title}</Text>
-  //         <Text>Informacoes de envio</Text>
-  //         <Text>Corpo do email</Text>
-  //       </View>
-  //     );
-  //   }
   return (
-    <View style={{ backgroundColor: "red" }}>
-      <Text style={{ fontSize: 20, color: "white" }}>{email.tittle}</Text>
-      <View style={{ flexDirection: "row" }}>
-        <Image style={styles.image} source={{ uri: email.picture }} />
-        <View>
-          <Text>
-            {email.from}...{email.time}
-          </Text>
-          <Text>Para mim </Text>
+    <View style={{ backgroundColor: "white", display: "flex", flex: 1 }}>
+      <View
+        style={{ alignItems: "center", flexDirection: "row", marginBottom: 30 }}
+      >
+        <Text style={{ fontSize: 20, color: "black", marginLeft: 20 }}>
+          {email.tittle + " "}
+        </Text>
+        <Text
+          style={{
+            backgroundColor: "#d3d3d3",
+            fontSize: 10,
+            borderRadius: 3,
+            // width: ,
+            // padding:5,
+            paddingHorizontal: 6,
+            textAlign: "center",
+          }}
+        >
+          Caixa de entrada
+        </Text>
+        <View
+          style={{ justifyContent: "center", flex: 1, alignItems: "flex-end", marginHorizontal:10 }}
+        >
+          <Icon
+            name={email.star ? "star" : "star-outline"}
+            size={30}
+            color={email.star ? "orange" : "gray"}
+          />
         </View>
       </View>
 
-      <WebView
-        source={{ html:"'"+email.body+"'"}}
-      />
+      <View style={{ flexDirection: "row", marginLeft: 20 }}>
+        <Image style={styles.image} source={{ uri: email.picture }} />
+        <View>
+          <View style={{ flexDirection: "row", alignItems: "baseline" }}>
+            <Text style={{ color: "black" }}>{email.from + " "}</Text>
+            <Text style={{ color: "black", fontSize: 8, padding: 1 }}>
+              {email.time}
+            </Text>
+          </View>
+          <Text style={{ color: "gray", fontSize: 8 }}>Para {email.to} </Text>
+        </View>
+      </View>
+      <View style={{ flex: 1, margin: 20 }}>
+        <WebView source={{ html: email.body }} />
+      </View>
     </View>
   );
 }
@@ -54,6 +75,6 @@ const styles = StyleSheet.create({
     width: 50,
     height: 50,
     borderRadius: 25,
-    margin: 10,
+    marginRight: 10,
   },
 });
